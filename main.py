fcbc06886bc6bd955a71397ecd4ced45cb2b2eee
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-04-22 13:44:23 -0400

    learning FastAPI Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# def main():
+#     print("Hello from fastapi-learning!")
+
+# if __name__ == "__main__":
+#     main()
+
+#-----------------------------------------------
+# import sys
+# print(sys.executable)
+#-----------------------------------------------
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "This is my first API"}
+
+@app.get("/name")
+def read_root1():
+    return {"message": "Hi Mithra"}
